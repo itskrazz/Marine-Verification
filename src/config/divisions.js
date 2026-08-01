cef19f3b7dabbaae003224f41480732571a9f5e2
SpongeBob SquarePants <phdgc17@gmail.com>
2026-07-31 23:08:59 -0400

    Refactor divisions and resolveDivision function
    Refactor divisions structure and update roleId references.

--- src/config/divisions.js
+++ src/config/divisions.js
@@ -1,16 +1,47 @@
-import { env } from './env.js';
+import { env } from "./env.js";
 
-const definitions = [
-  { key: 'HQMC', roleId: env.ROLE_HQMC, teamName: 'Headquarters Marine Corps', priority: 100 },
-  { key: 'MARSOC', roleId: env.ROLE_MARSOC, teamName: 'Marine Forces Special Operations Command', priority: 90 },
-  { key: 'TECOM', roleId: env.ROLE_TECOM, teamName: 'Training and Education Command', priority: 80 },
-  { key: 'MCRD', roleId: env.ROLE_MCRD, teamName: 'Marine Corps Recruit Depot', priority: 70 },
-  { key: 'I_MEF', roleId: env.ROLE_I_MEF, teamName: 'I Marine Expeditionary Force', priority: 60 }
-];
+export const divisions = Object.freeze([
+  {
+    key: "HQMC",
+    priority: 100,
+    discordRoleId: env.ROLE_HQMC,
+    robloxTeam: env.TEAM_HQMC
+  },
+  {
+    key: "MARSOC",
+    priority: 90,
+    discordRoleId: env.ROLE_MARSOC,
+    robloxTeam: env.TEAM_MARSOC
+  },
+  {
+    key: "TECOM",
+    priority: 80,
+    discordRoleId: env.ROLE_TECOM,
+    robloxTeam: env.TEAM_TECOM
+  },
+  {
+    key: "MCRD",
+    priority: 70,
+    discordRoleId: env.ROLE_MCRD,
+    robloxTeam: env.TEAM_MCRD
+  },
+  {
+    key: "I_MEF",
+    priority: 60,
+    discordRoleId: env.ROLE_I_MEF,
+    robloxTeam: env.TEAM_I_MEF
+  }
+].filter((division) => division.discordRoleId));
 
-export const DIVISIONS = Object.freeze(
-  definitions.filter((division) => typeof division.roleId === 'string' && division.roleId.length > 0)
-);
+export function resolveDivision(memberRoleIds) {
+  const matches = divisions
+    .filter((division) => memberRoleIds.has(division.discordRoleId))
+    .sort((a, b) => b.priority - a.priority);
 
-export const DEFAULT_TEAM = 'Civilian';
-export const PERSONNEL_TEAM = 'Marine Corps Personnel';
+  return matches[0] ?? {
+    key: "DEFAULT",
+    priority: 0,
+    discordRoleId: null,
+    robloxTeam: env.TEAM_DEFAULT
+  };
+}
